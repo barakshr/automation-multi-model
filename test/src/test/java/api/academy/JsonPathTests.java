5998ed134438a7e13ab9a75846fff6884a23704d
barakshamir <barak20000@gmail.com>
2023-09-06 19:30:26 +0300

    -

--- test/src/test/java/api/academy/JsonPathTests.java
+++ test/src/test/java/api/academy/JsonPathTests.java
@@ -10,10 +10,10 @@ public class JsonPathTests {
     @Test
     public void jsonPathTest() {
         String payloadJson = Payload.readPayloadJson("coursePrice.json");
-        String b = payload.CoursePrice();
         JsonPath js = new JsonPath(payloadJson);
         int actualCount = js.getInt("courses.size()");
         String titleFirstCourse = js.get("courses[2].title");
+
         Assert.assertEquals(actualCount,4);
         Assert.assertEquals(titleFirstCourse,"RPA");
     }
