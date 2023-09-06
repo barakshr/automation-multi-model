fe5f97f291f9e3a35bf9ac0c7c42a9eefe599fcd
barakshamir <barak20000@gmail.com>
2023-09-06 19:23:42 +0300

    -

---
+++ test/src/test/java/api/academy/JsonPathTests.java
@@ -0,0 +1,20 @@
+package api.academy;
+
+import com.team.api.properties.Payload;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class JsonPathTests {
+
+    @Test
+    public void jsonPathTest() {
+        String payloadJson = Payload.readPayloadJson("coursePrice.json");
+        String b = payload.CoursePrice();
+        JsonPath js = new JsonPath(payloadJson);
+        int actualCount = js.getInt("courses.size()");
+        String titleFirstCourse = js.get("courses[2].title");
+        Assert.assertEquals(actualCount,4);
+        Assert.assertEquals(titleFirstCourse,"RPA");
+    }
+}
