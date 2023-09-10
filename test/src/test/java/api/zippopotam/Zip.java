f622d0af2d3869c9b122930ee54b5161eb45873c
barakshamir <barak20000@gmail.com>
2023-09-10 22:04:43 +0300

    -

--- test/src/test/java/api/zippopotam/Zip.java
+++ test/src/test/java/api/zippopotam/Zip.java
@@ -2,14 +2,22 @@ package api.zippopotam;
 
 
 import api.BaseTest;
+import com.google.gson.JsonObject;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 
 
 public class Zip extends BaseTest {
 
     @Test
-    public void getZipCode(){
-      String body=  RestAssured.given().get("http://api.zippopotam.us/us/90210").asString();
+    public void pathParam(){
+      String body=  RestAssured.given()
+              .log().all()
+              .pathParams("key","90210").get("http://api.zippopotam.us/us/{key}")
+              .then()
+              .log().all()
+              .extract().body().asString();
+        JsonPath jsonPath= new JsonPath(body);
     }
 }
